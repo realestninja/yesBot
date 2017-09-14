525474568cf1867a1a1047b5a9c5cce1020e2d56
nikhil <the@realest.ninja>
2017-09-14 23:07:26 +0200

    Add PID output

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 var token = '';
+var process = require('process');
 var TelegramBot = require('node-telegram-bot-api');
 var bot = new TelegramBot(token, {polling: true});
+var stickerToSend = 'BQADBAADhgADgaJbBupIQEcw4qw1Ag';
 
 //any kind of message
 bot.on('message', function(json) {
-  var chatId = json.chat.id;
+  var chatID = json.chat.id;
 
   if (json.hasOwnProperty('text')) {
     var message = json.text;
     var lastChar = message.substr(message.length - 1)
 
-    if (lastChar == '?') {
-      bot.sendSticker(chatId, 'BQADBAADhgADgaJbBupIQEcw4qw1Ag');
+    if (lastChar === '?') {
+      bot.sendSticker(chatID, stickerToSend);
     }
   }
-
 });
 
-console.log('bot running');
+console.log('bot running - pid: ' +  process.pid);
